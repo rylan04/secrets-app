fc7f160a13bfc926c829309b5bd2435575aadac0
rylan04 <rylan.sykes@hotmail.com>
2020-07-19 14:32:46 -0400

    Fix port bug

--- app.js
+++ app.js
@@ -170,6 +170,6 @@ app.post("/login", function(req, res) {
   });
 });
 
-app.listen(process.env.PORT, 3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Listening on port 3000");
 });
